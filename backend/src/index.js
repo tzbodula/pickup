833794d697379761e1089bfb40aa04e5f8fce1d5
Cngo2019 <canngo215@gmail.com>
2022-10-12 17:15:41 -0400

    added some commnets

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,8 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded());
 
+// Any requests made to localhost:3000/users will be delegated to the users.js code.
+// Any requests made to localhost:3000/events will be delegated to the users.js code.
 app.use('/users', usersRoute);
 app.use('/events', eventsRoute);
 
