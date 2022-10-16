f0db0cd17b37a74b54b354a2a6f85a64f2921aab
Cngo2019 <canngo215@gmail.com>
2022-10-16 12:37:01 -0400

    fixed some bugs

--- backend/src/config/DatabaseConfig.js
+++ backend/src/config/DatabaseConfig.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
-const dotnev = require('dotenv');
-dotnev.config({path:'.env'});
+require('dotenv').config();
+
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -4,6 +4,7 @@ const db = require('../config/DatabaseConfig')
 const router = Router();
 
 router.get('/', async (request, response) => {
+    // Query that gets all the users
     let users = await db.query('SELECT * FROM accounts');
     response.send(users[0]);
 })
