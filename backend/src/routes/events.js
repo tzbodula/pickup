53be59123650e0395029c754008257247ee1cb9f
Benjamin Gambill <bgambill@uncc.edu>
2022-10-16 20:02:03 -0400

    Added GET endpoint to filter events by sport

--- backend/src/routes/events.js
+++ backend/src/routes/events.js
@@ -1,9 +1,15 @@
 const { Router } = require('express');
+const db = require('../config/databaseConfig')
 
 const router = Router();
 
 router.get('/', (request, response) => {
     response.send("Welcome to the Events route");
+})
+
+router.get('/sport', async (request, response) => {
+    let filteredEvents = await db.query("SELECT* FROM pickup_events WHERE sport_id = " + db.escape(request.query.sport));
+    response.send(filteredEvents[0]);
 })
 
 module.exports = router;
