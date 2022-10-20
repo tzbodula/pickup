7b13b4d435623726fb452d53e9ad3e1a3456e320
Cngo2019 <canngo215@gmail.com>
2022-10-20 14:26:32 -0400

    can now create an account

--- backend/src/routes/events.js
+++ backend/src/routes/events.js
@@ -4,9 +4,12 @@ const db = require('../config/databaseConfig')
 const router = Router();
 // Sends back list of events filtered by the sport entered in as query parameter
 router.get('/sport', async (req, res) => {
-    let events = await db.query(
-    `SELECT * FROM pickup_events 
-        WHERE sport_id = ` + db.escape(req.query.sport));
+
+    const query = `SELECT * FROM pickup_events WHERE sport_id = ?`
+
+    let events = await db.query(query, [req.query.sport], (err, res) => {
+        //handle any errors
+    });
 
     if (events[0].length == 0) {
         return res.status(400).send("Not found");
@@ -16,10 +19,12 @@ router.get('/sport', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-    let events = await db.query(
-    `SELECT * FROM pickup_events 
-        WHERE event_id = ` + db.escape(req.params.id)
-    );
+
+    const query = `SELECT * FROM pickup_events WHERE event_id = ?`
+
+    let events = await db.query(query, [req.params.id], (err, res) => {
+        //handle any errors
+    });
 
     if (events[0].length == 0) {
         return res.status(400).send("Not found");
@@ -29,10 +34,14 @@ router.get('/:id', async (req, res) => {
 });
 
 router.get('/:id/players', async (req, res) => {
-    let events = await db.query(
-    `SELECT accounts.account_username, accounts.games_joined, accounts.games_attended FROM player_event 
-        JOIN accounts ON player_event.account_id = accounts.account_id 
-        WHERE event_id = ` + db.escape(req.params.id));
+
+    const query = `SELECT accounts.account_username, accounts.games_joined, accounts.games_attended FROM player_event 
+    JOIN accounts ON player_event.account_id = accounts.account_id 
+    WHERE event_id = ? `
+
+    let events = await db.query(query, [req.params.id], (err, res) => {
+        //handle any errors
+    });
 
     if (events[0].length == 0) {
         return res.status(400).send("Not found");
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -3,10 +3,39 @@ const db = require('../config/databaseConfig')
 
 const router = Router();
 
+router.post('/', async (req, res) => {
+    // TODO hash the password before saving
+    const userToAdd = [
+        req.body.first_name,
+        req.body.last_name,
+        req.body.username,    
+        req.body.password,
+        req.body.email,
+        0,
+        0,
+        0
+    ];
+    
+    // TODO: check if UN already exists, 
+    // TODO: Check if email already exists
+    const insertStatement =
+        `INSERT INTO accounts
+            (first_name, last_name, account_username, account_password, email, games_joined, games_attended, rating)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+
+    await db.query(insertStatement, userToAdd);
+
+    return res.status(200).send('Successful Creation');
+})
+
 router.get('/:id', async (req, res) => {
-    let users = await db.query(
-    `SELECT account_username, account_id, first_name, last_name FROM accounts 
-        WHERE accounts.account_id = ` + db.escape(req.params.id));
+
+    const query = `SELECT account_username, account_id, first_name, last_name FROM accounts 
+    WHERE accounts.account_id = ?`
+
+    const users = await db.query(query, [req.params.id], (err, res) => {
+        // Handle any errors
+    });
 
     if (users[0].length == 0) {
         return res.status(400).send("Not found");
@@ -17,10 +46,14 @@ router.get('/:id', async (req, res) => {
 
 // Sends back list of ids of all of the user's favorite sports
 router.get('/:id/sports', async (req, res) => {
-    let favoriteSportsQuery = await db.query(
-    `SELECT sports.sport_name, sports.sport_id FROM player_sport_favorite 
-        JOIN sports ON player_sport_favorite.sport_id = sports.sport_id 
-        WHERE account_id = ` + db.escape(req.params.id));
+
+    const query = `SELECT sports.sport_name, sports.sport_id FROM player_sport_favorite 
+    JOIN sports ON player_sport_favorite.sport_id = sports.sport_id 
+    WHERE account_id = ? `
+
+    let favoriteSportsQuery = await db.query(query, [req.params.id], (err, res) => {
+        // Handle any errors
+    });
     
     if (favoriteSportsQuery[0].length == 0) {
         return res.status(400).send("Not found");
