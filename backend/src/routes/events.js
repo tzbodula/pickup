2f5128082c42ec8c6a773b5c0f48fe67cf527c58
Benjamin Gambill <bgambill@uncc.edu>
2022-10-22 20:09:08 -0400

    Deletion of events

--- backend/src/routes/events.js
+++ backend/src/routes/events.js
@@ -33,6 +33,23 @@ router.get('/:id', async (req, res) => {
     return res.status(200).send(events[0]);
 });
 
+// Event deletion
+router.get('/:id/delete', async (req, res) => {
+    // Possibly better solution here (like a cascade delete) but I'm not sure
+    const query1 = `DELETE FROM player_event WHERE event_id = ?`
+    const query2 = `DELETE FROM pickup_events WHERE event_id = ?`
+
+    await db.query(query1, [req.params.id], (err, res) => {
+        //handle any errors
+    });
+    await db.query(query2, [req.params.id], (err, res) => {
+        //handle any errors
+    });
+    
+    //return res.status(200).send(Event deleted.);
+    return res.redirect('/events');
+});
+
 router.get('/:id/players', async (req, res) => {
 
     const query = `SELECT accounts.account_username, accounts.games_joined, accounts.games_attended FROM player_event 
