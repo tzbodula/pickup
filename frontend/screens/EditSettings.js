f10241e67f3ce4a1f0ae0c394d931e6869aa118a
Benjamin Gambill <bgambill@uncc.edu>
2022-10-25 21:58:53 -0400

    Changes to EditSettings Page

--- frontend/screens/EditSettings.js
+++ frontend/screens/EditSettings.js
@@ -1,74 +1,76 @@
 import * as React from "react";
-import { Image, StyleSheet, Text, View, Pressable } from "react-native";
+import { Image, StyleSheet, Text, View, Pressable, SafeAreaView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { Dimensions } from 'react-native';
+
 
 const EditSettings = () => {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.editSettingsView}>
-      <View style={styles.footerView}>
-        <View style={styles.singleTabView}>
-          <View style={styles.iconAndText}>
+    <SafeAreaView style={styles.editSettingsView}>
+      <SafeAreaView style={styles.footerView}>
+        <SafeAreaView style={styles.singleTabView}>
+          <SafeAreaView style={styles.iconAndText}>
             <Image
               style={styles.homeIcon}
               resizeMode="cover"
               source={require("../assets/home3.png")}
             />
             <Text style={[styles.text, styles.mt2]}>Account</Text>
-          </View>
-        </View>
+          </SafeAreaView>
+        </SafeAreaView>
         <Pressable
           style={styles.singleTabPressable}
           onPress={() => navigation.navigate("Friends")}
         >
-          <View style={styles.iconAndText1}>
+          <SafeAreaView style={styles.iconAndText1}>
             <Image
               style={styles.userIcon}
               resizeMode="cover"
               source={require("../assets/user.png")}
             />
             <Text style={[styles.text1, styles.mt2]}>Friends</Text>
-          </View>
+          </SafeAreaView>
         </Pressable>
         <Pressable
           style={styles.singleTabPressable1}
           onPress={() => navigation.navigate("Map")}
         >
-          <View style={styles.iconAndText2}>
+          <SafeAreaView style={styles.iconAndText2}>
             <Image
               style={styles.compassIcon}
               resizeMode="cover"
               source={require("../assets/compass.png")}
             />
             <Text style={[styles.text2, styles.mt2]}>Map</Text>
-          </View>
+          </SafeAreaView>
         </Pressable>
         <Pressable
           style={styles.framePressable}
           onPress={() => navigation.navigate("CreateEvent")}
         >
           <Image
-            style={styles.ellipseIcon}
+            style={styles.addEventCircle}
             resizeMode="cover"
             source={require("../assets/ellipse-1.png")}
           />
-          <Text style={styles.text3}>+</Text>
+          <Text style={styles.addEventPlus}>+</Text>
         </Pressable>
         <Pressable
           style={styles.singleTabPressable2}
           onPress={() => navigation.navigate("MainPage")}
         >
-          <View style={styles.iconAndText3}>
+          <SafeAreaView style={styles.iconAndText3}>
             <Image
               style={styles.searchIcon}
               resizeMode="cover"
               source={require("../assets/search4.png")}
             />
             <Text style={[styles.text4, styles.mt2]}>Events</Text>
-          </View>
+          </SafeAreaView>
         </Pressable>
-      </View>
+      </SafeAreaView>
       <Pressable
         style={styles.vectorPressable}
         onPress={() => navigation.navigate("ProfileUser")}
@@ -79,7 +81,7 @@ const EditSettings = () => {
           source={require("../assets/vector9.png")}
         />
       </Pressable>
-      <View style={styles.buttonView}>
+      <SafeAreaView style={styles.buttonView}>
         <Image
           style={styles.leadingIcon}
           resizeMode="cover"
@@ -91,7 +93,7 @@ const EditSettings = () => {
           resizeMode="cover"
           source={require("../assets/trailing-icon10.png")}
         />
-      </View>
+      </SafeAreaView>
       <Pressable
         style={styles.buttonPressable}
         onPress={() => navigation.navigate("Login")}
@@ -137,7 +139,7 @@ const EditSettings = () => {
         resizeMode="cover"
         source={require("../assets/check.png")}
       />
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -274,26 +276,27 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     overflow: "hidden",
   },
-  ellipseIcon: {
+  addEventCircle: {
     position: "absolute",
     height: "100%",
     width: "100%",
     top: "0%",
     right: "0%",
     bottom: "0%",
-    left: "0%",
+    left: "30%",
     maxWidth: "100%",
     overflow: "hidden",
     maxHeight: "100%",
   },
-  text3: {
+  addEventPlus: {
+    paddingTop: 20,
     position: "absolute",
     height: "47.46%",
     width: "49.15%",
     top: "27.12%",
     right: "25.42%",
     bottom: "25.42%",
-    left: "25.42%",
+    left: "53.42%",
     fontSize: 48,
     lineHeight: 18,
     fontFamily: "Arsenal",
@@ -351,9 +354,9 @@ const styles = StyleSheet.create({
   },
   footerView: {
     position: "absolute",
-    top: 729,
+    top: Dimensions.get('window').height * 0.85,
     left: 0,
-    width: 375,
+    width: Dimensions.get('window').width,
     height: 68,
   },
   icon: {
@@ -529,10 +532,10 @@ const styles = StyleSheet.create({
   },
   accountSettingsText: {
     position: "absolute",
-    top: 29,
+    top: 15,
     left: 45,
     fontSize: 15,
-    lineHeight: 14,
+    lineHeight: 25,
     fontFamily: "GearUp",
     color: "#000",
     textAlign: "left",
@@ -591,12 +594,13 @@ const styles = StyleSheet.create({
     height: 16,
   },
   editSettingsView: {
+    top: 35,
     position: "relative",
     backgroundColor: "#fff",
     flex: 1,
-    width: "100%",
-    height: 812,
     overflow: "hidden",
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
 
