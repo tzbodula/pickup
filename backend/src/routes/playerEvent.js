c237108d6be735b3289b8917abcadbfa5ce71655
Samuel King <sking80@uncc.edu>
2022-11-10 19:32:37 -0500

    Moved to currentUser

--- backend/src/routes/playerEvent.js
+++ backend/src/routes/playerEvent.js
@@ -71,27 +71,6 @@ router.delete('/:id/leave', (req,res) => {
     }) 
 })
 
-router.get('/:id/getEvents',  (req, res) => {
-    
-    //console.log("We are getting the command still");
-
-    const query = `SELECT * FROM player_event
-    JOIN pickup_events ON pickup_events.event_id = player_event.event_id
-    WHERE player_event.account_id = 1
-    ;`
-
-    db.query(query, [req.params.id], (err, result) => {
-        //handle any errors
-
-        if (result === undefined || result.length == 0) {
-            return res.status(400).send({message: "Not found", status: 400});
-        }
-    
-        return res.status(200).send({data: result, status: 200});
-    });
-    
-});
-
 router.get('/:id/players',  (req, res) => {
 
     const query = `SELECT accounts.account_id, accounts.account_username, accounts.games_joined, accounts.games_attended FROM player_event 
